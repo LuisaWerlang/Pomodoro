b3782248e5a81203dfefdf387323516fe8277375
Luisa Werlang <luisa_werlang@hotmail.com>
2022-04-27 23:27:05 -0300

    pomodoro

--- app/src/main/java/com/example/pomodoro/QuizzFragment.java
+++ app/src/main/java/com/example/pomodoro/QuizzFragment.java
@@ -1,12 +1,27 @@
 package com.example.pomodoro;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import com.example.pomodoro.utils.DatabaseHelper;
+import com.tooltip.Tooltip;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +34,11 @@ public class QuizzFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private DatabaseHelper helper;
+    private List<Map<String, Object>> questions;
+    private final String[] from = {"question"};
+    private final int[] to = {R.id.tv_question};
+    private ListView questions_list;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -59,6 +79,88 @@ public class QuizzFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quizz, container, false);
+        View view = inflater.inflate(R.layout.fragment_quizz, container, false);
+
+        helper = new DatabaseHelper(getActivity());
+        String query = "SELECT * FROM question";
+        questions = listQuestions(query);
+
+        // Aqui você instancia sua ListView
+        questions_list = view.findViewById(R.id.lv_questions);
+
+        SimpleAdapter adapter = new SimpleAdapter(getActivity(), questions, R.layout.quizz_list, from, to);
+        questions_list.setAdapter(adapter);
+        questions_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //EditText comments = adapterView.findViewById(R.id.et_observation);
+                //questions.get(i).put("comment",comments.getText().toString());
+            }
+        });
+
+        final Tooltip[] tooltip = new Tooltip[1];
+        final int[] tooltip_ok = {2};
+
+        ImageButton help = view.findViewById(R.id.help);
+        help.setOnClickListener(view1 -> {
+            if(tooltip_ok[0] == 1) {
+                tooltip[0].dismiss();
+                tooltip_ok[0] = 2;
+            }
+            else {
+                tooltip_ok[0] = 1;
+                tooltip[0] = new Tooltip.Builder(help)
+                        .setText("Legenda:\n " +
+                                "CT - Concordo Totalmente\n " +
+                                " C - Concordo\n " +
+                                "ND - Não concordo nem discordo\n " +
+                                " D - Discordo\n" +
+                                "DT - Discordo Totalmente")
+                        .setGravity(Gravity.START)
+                        .show();
+            }
+        });
+
+        Button save = view.findViewById(R.id.save_questions);
+        save.setOnClickListener(view1 -> {
+            saveQuizz();
+        });
+
+        return view;
+    }
+
+    private void saveQuizz() {
+        for(int i = 0; i < questions_list.getCount(); i++) {
+            //if(questions_list.isItemChecked(i) == true) {
+            //Log.e("teste", questions.get(i).toString());
+            //Log.e("teste",""+questions_list.getItemAtPosition(i));
+            //}
+            //View convertView = questions_list.getChildAt(i);
+            //final EditText edAnalise = convertView.findViewById(R.id.et_observation);
+            //edAnalise.getText().toString()
+            //Log.e("teste",""+convertView);
+        }
+    }
+
+    private List<Map<String, Object>> listQuestions(String query) {
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        cursor.moveToFirst();
+        questions = new ArrayList<>();
+        for(int i=0; i<cursor.getCount(); i++) {
+            Map<String, Object> item = new HashMap<>();
+            String question = cursor.getString(1);
+            item.put("question", question);
+            questions.add(item);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return questions;
+    }
+
+    @Override
+    public void onDestroy() {
+        helper.close();
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/pomodoro/utils/DatabaseHelper.java
+++ app/src/main/java/com/example/pomodoro/utils/DatabaseHelper.java
@@ -15,6 +15,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS user (" +
+                "id INTEGER PRIMARY KEY NOT NULL," +
+                "user TEXT," +
+                "password TEXT)");
+
         db.execSQL("CREATE TABLE IF NOT EXISTS activities (" +
                             "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
                             "name TEXT, " +
@@ -41,6 +47,36 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 "hour int, " +
                 "hour_notify int, " +
                 "activity_name TEXT);");
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS question (" +
+                "id INTEGER PRIMARY KEY NOT NULL, " +
+                "question TEXT);");
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS answer (" +
+                "id INTEGER PRIMARY KEY NOT NULL, " +
+                "user TEXT," +
+                "question TEXT," +
+                "CT INTEGER," +
+                "C INTEGER," +
+                "ND INTEGER," +
+                "D INTEGER," +
+                "DT INTEGER," +
+                "observation TEXT)");
+
+        String sql2 = "INSERT INTO question " +
+                "(id, question) " +
+                "VALUES " +
+                "(1, 'Realizo as atividades da função satisfatoriamente, apresentando bons resultados, em tempo adequado, sem retrabalho e não conformidades.')," +
+                "(2, 'Gerencio bem o meu tempo, fazendo gestão de prioridades no trabalho.')," +
+                "(3, 'Demonstro entusiasmo, determinação e motivação na execução das atividades, contagiando e motivando o restante da equipe de trabalho em direção aos resultados esperados.')," +
+                "(4, 'Eu me sinto no domínio das coisas em função de preparação e planejamento cuidadoso para vencer as atividades e tarefas durante o trabalho.')," +
+                "(5, 'Eu gasto muito tempo em atividades secundárias durante o trabalho como correspondências supérfluas.')," +
+                "(6, 'Eu sinto que desperdiço muito tempo durante o trabalho.')," +
+                "(7, 'As demandas de outras pessoas, durante o trabalho, desviam-me do meu caminho para atingir objetivos e metas essenciais.')," +
+                "(8, 'Eu corro o dia inteiro, mas faço poucas coisas que contribuam para as principais prioridades da minha organização no trabalho.')," +
+                "(9, 'Eu gasto muito do meu tempo, durante o trabalho, em atividades de pouca relevância para as minhas prioridades, mas que requerem atenção imediata (por exemplo: interrupções inúteis, reuniões sem importância, telefonemas e e-mails dispensáveis).')," +
+                "(10, 'Eu consigo manter o foco e a atenção enquanto realizo as minhas atividades, sem distrair-me frequentemente com o celular, durante o trabalho.')";
+        db.execSQL(sql2);
     }
 
     @Override
